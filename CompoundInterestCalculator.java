0e8b9ce23af9db6d906f2d8bcd847b97ddcc876d
Keshav <keshav14kumar2004@gmail.com>
2025-09-08 10:38:05 +0530

    Compound Interest Calculator

---
+++ CompoundInterestCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CompoundInterestCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please Enter P (Principle) : ");
+        double P = scanner.nextDouble();
+
+        System.out.print("Please Enter R (Rate of Interest) : ");
+        double R = scanner.nextDouble();
+
+        System.out.print("Please Enter T (Time) : ");
+        double T = scanner.nextDouble();
+
+        double C_I = P * ( 1 + R / 100) * T;
+
+        System.out.println("Compound Interest is : " + C_I);
+
+    }
+}
