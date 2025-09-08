86e5077acf9ca9243b3352815c1ae551bb1242b6
Keshav <keshav14kumar2004@gmail.com>
2025-09-09 01:07:32 +0530

    Unit Of Temprature Change Formula

---
+++ TempratureUnitChange.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TempratureUnitChange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        Here we change the Unit of Temprature, Fahrenheit to celsius
+        System.out.print("Enter the Temprature in Farhenheit : ");
+        double F = scanner.nextDouble();
+
+        double C = (F-32)*5/9;
+
+        System.out.print("If Temprature is " + F + " Degree Farhenheit then in Celsius it will be : " + C + " Degree Celsius" );
+
+    }
+}
