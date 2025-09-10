b5a626b65519e95000617f904fac43935fffd4cb
Keshav <keshav14kumar2004@gmail.com>
2025-09-10 19:19:11 +0530

    Function

---
+++ 19 _Function.java
@@ -0,0 +1,24 @@
+class Function {
+    public static void main(String[] args) {
+
+        Pattern();
+
+    }
+
+    public static void Pattern() {
+
+        int rows = 0;
+        while( rows < 5 ){
+            System.out.print("*");
+
+            int i = 0;
+            while (i < rows){
+                System.out.print(" *");
+                i++;
+            }
+            System.out.println();
+            rows++;
+        }
+
+    }
+}
